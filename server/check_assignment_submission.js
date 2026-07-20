5c6647457b4752a0ec3c446f2399e96f03a6f616
Edmin Developer <developer@edmin.com>
2026-07-20 12:33:49 +0500

    Fix submission downloads: convert ZIP to PDF and update Supabase storage script

--- server/check_assignment_submission.js
+++ server/check_assignment_submission.js
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  const sub = await prisma.assignmentsubmission.findFirst();
-  console.log('Submission:', sub);
+  const sub = await prisma.assignmentsubmission.findMany();
+  console.log('Submissions:', sub);
 }
 main().catch(console.error).finally(() => prisma.$disconnect());
