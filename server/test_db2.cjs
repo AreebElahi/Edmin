0cc5f792c289857a8197e83f548aa54e653aa7d2
Areeb Elahi <106818935+AreebElahi@users.noreply.github.com>
2026-07-21 05:38:40 +0500

    chore: remove test files

--- server/test_db2.cjs
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  const users = await prisma.user.findMany({
-    where: { userid: { in: [43, 36] } }
-  });
-  console.log('Users:', JSON.stringify(users, null, 2));
-}
-main().finally(() => prisma.$disconnect());
